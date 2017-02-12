d770510cdc87582b44903e4f7f72144c8ab72d4f
zerolime <crazy.kucher@gmail.com>
2017-02-12 16:54:04 +0300

    --

--- python/test3.py
+++ python/test3.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# чётное ли число
-
-a = (-17)
-if a % 2 == 0:
+a = ['']
+if a:
     print ('число чётное')
-    print (a**2)
 else:
     print ('число нечётное')
-    print (a**2)
-print ('END')
